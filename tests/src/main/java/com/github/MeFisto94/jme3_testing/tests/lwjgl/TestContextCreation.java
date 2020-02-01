1d082da5bd5e591a70d56a7dc9e700d069b31f8f
MeFisto94 <MeFisto94@users.noreply.github.com>
2020-02-01 02:38:11 +0100

    Don't wait on context disposal, because when an exception is thrown in the main thread, this could mean an infinite wait under lwjgl3.

--- tests/src/main/java/com/github/MeFisto94/jme3_testing/tests/lwjgl/TestContextCreation.java
+++ tests/src/main/java/com/github/MeFisto94/jme3_testing/tests/lwjgl/TestContextCreation.java
@@ -42,7 +42,7 @@ public abstract class TestContextCreation {
                 }
             }
 
-            app.stop(true);
+            app.stop(false);
             app = null;
         }
     }
